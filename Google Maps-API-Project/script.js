0e244811b2f1ffdf5f1ca179bab2a2f6991c074c
Matthew Erich <matthewerich1992@gmail.com>
2016-08-13 18:57:07 -0700

    Google Maps Api location changed
    Changed location from New York to San Francisco and linked new location
    data for markers.
    
    Changed and updated API key to be consistent with my own

--- Google Maps-API-Project/script.js
+++ Google Maps-API-Project/script.js
@@ -4,20 +4,18 @@ var map;
       function initMap() {
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7413549, lng: -73.9980244},
+          center: {lat: 37.77493, lng:-122.419416},
           zoom: 13,
           mapTypeControl: false
         });
         // These are the real estate listings that will be shown to the user.
         // Normally we'd have these in a database instead.
         var locations = [
-          {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-        ];
+          {title: "Golden-Gate Park", location: {lat: 37.76904, lng:-122.483519}},
+          {title: "The Fillmore", location: {lat:37.787966,lng:-122.433681}},
+          {title: "Sightglass", location: {lat:37.776981,lng:-122.408571}},
+          {title:"MOMA", location: {lat:37.785718,lng:-122.401051}}
+          ];
         var largeInfowindow = new google.maps.InfoWindow();
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < locations.length; i++) {
@@ -38,8 +36,9 @@ var map;
             populateInfoWindow(this, largeInfowindow);
           });
         }
-        document.getElementById('show-listings').addEventListener('click', showListings);
-        document.getElementById('hide-listings').addEventListener('click', hideListings);
+        document.getElementById('show-listings').addEventListener('click', showListings); //Created 'showListings function to Show Markers'
+
+        document.getElementById('hide-listings').addEventListener('click', hideListings); // Created 'hideListings function to HideButtons'
       }
       // This function populates the infowindow when the marker is clicked. We'll only allow
       // one infowindow which will open at the marker that is clicked, and populate based
